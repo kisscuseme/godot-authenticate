ac5ad2d97ba1aa464a29949d4d4496755e77d81e
kyle <kimsm0408@naver.com>
2022-06-25 02:20:46 +0900

    add creating account function

--- Scenes/Main/Authenticate.gd
+++ Scenes/Main/Authenticate.gd
@@ -23,25 +23,64 @@ func _Peer_Disconnected(gateway_id):
 
 remote func AuthenticatePlayer(username, password, player_id):
 	print("authentication request received")
-	var token
 	var gateway_id = get_tree().get_rpc_sender_id()
+	var hashed_password
+	var token
 	var result
 	print("Starting authentication")
 	if not PlayerData.PlayerIDs.has(username):
 		print("User not recognized")
 		result = false
-	elif not PlayerData.PlayerIDs[username].Password == password:
-		print("Incorrect password")
-		result = false
 	else:
-		print("Successful authentication")
-		result = true
+		var retrieved_salt = PlayerData.PlayerIDs[username].Salt
+		hashed_password = GenerateHashedPassword(password, retrieved_salt)
+		if not PlayerData.PlayerIDs[username].Password == hashed_password:
+			print("Incorrect password")
+			result = false
+		else:
+			print("Successful authentication")
+			result = true
 		
-		randomize()
-		token = str(randi()).sha256_text() + str(OS.get_unix_time())
-		print("token: " + token)
-		var gameserver = "GameServer1" # 로드 밸런싱을 통해 선택되도록 수정되어야 함
-		GameServerHub.DistributeLoginToken(token, gameserver)
+			randomize()
+			token = str(randi()).sha256_text() + str(OS.get_unix_time())
+			print("token: " + token)
+			var gameserver = "GameServer1" # 로드 밸런싱을 통해 선택되도록 수정되어야 함
+			GameServerHub.DistributeLoginToken(token, gameserver)
 		
 	print("authentication result send to gateway server")
 	rpc_id(gateway_id, "AuthenticationResults", result, player_id, token)
+
+remote func CreateAccount(username, password, player_id):
+	var gateway_id = get_tree().get_rpc_sender_id()
+	var result
+	var message
+	if PlayerData.PlayerIDs.has(username):
+		result = false
+		message = 2
+	else:
+		result = true
+		message = 3
+		var salt = GenerateSalt()
+		var hashed_password = GenerateHashedPassword(password, salt)
+		PlayerData.PlayerIDs[username] = {"Password": hashed_password, "Salt": salt}
+		PlayerData.SavePlayerIds()
+	
+	rpc_id(gateway_id, "CreateAccountResults", result, player_id, message)
+
+func GenerateSalt():
+	randomize()
+	var salt = str(randi()).sha256_text()
+	print("Salt: " + salt)
+	return salt
+
+func GenerateHashedPassword(password, salt):
+	print(str(OS.get_system_time_msecs()))
+	var hashed_password = password
+	var rounds = pow(2, 18)
+	print("hashed password as input: " + hashed_password)
+	while rounds > 0:
+		hashed_password = (hashed_password + salt).sha256_text()
+		rounds -= 1
+	print("final hashed password: " + hashed_password)
+	print(str(OS.get_system_time_msecs()))
+	return hashed_password
--- Scenes/Singletons/PlayerData.gd
+++ Scenes/Singletons/PlayerData.gd
@@ -4,7 +4,13 @@ var PlayerIDs: Dictionary
 
 func _ready():
 	var player_data_file = File.new()
-	player_data_file.open("res://Data/PlayerData.json", File.READ)
+	var err = player_data_file.open("res://Data/PlayerIds.json", File.READ)
 	var player_data_json = JSON.parse(player_data_file.get_as_text())
 	player_data_file.close()
 	PlayerIDs = player_data_json.result
+
+func SavePlayerIds():
+	var save_file = File.new()
+	save_file.open("res://Data/PlayerIds.json", File.WRITE)
+	save_file.store_line(to_json(PlayerIDs))
+	save_file.close()
